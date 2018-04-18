d77e4efe9df887d0bc3dcc8d3932d9a22bf6076d
harvey <harveyaot@gmail.com>
2018-04-18 21:25:13 +0800

    updates

--- demo/src/App.js
+++ demo/src/App.js
@@ -6,9 +6,14 @@ import Paragraphs from './Paragraph.js'
 import axios from 'axios';
 import {Col, Row, Grid} from 'react-bootstrap'
 
+import Button from 'material-ui/Button';
+import Input from 'material-ui/Input'
+
 var headers = {
          'Content-Type': 'application/json',
 }      
+
+var urlPrefix = "http://stcvm-linux22:5904"
 
 class Form extends Component{
   constructor(props){
@@ -17,31 +22,36 @@ class Form extends Component{
     this.state = {'url':"", 'show':true,
               judged_num:null,
               total_num:null,
+              stats:null,
+              querysetIdx:0,
     };
     this.randomUrl = this.randomUrl.bind(this);
-    this.onhideBoilerPlate = this.onhideBoilerPlate.bind(this);
     this.handleFormChange = this.handleFormChange.bind(this);
     this.updateStatistic = this.updateStatistic.bind(this);
   }
 
   randomUrl(type){
-    axios.get("/random_one?type=" + type).then((res) => 
+    var json_d = {"type":type,"tagidx":this.state.querysetIdx, "signature":this.refs.signature?this.refs.signature.value:""}
+    var that = this
+    axios.post(urlPrefix + "/random_one", json_d, headers).then((res) =>
         {
-          this.setState({"url":res.data.url});
-          this.input.value = res.data.url;
+          that.setState({"url":res.data.url});
+          that.input.value = res.data.url;
         }
     )
   }
 
   updateStatistic(){
-    axios.get("/statistic").then((res) => 
+    var json_d = {"signature":this.refs.signature?this.refs.signature.value:""}
+    axios.post(urlPrefix + "/statistic", json_d, headers).then((res) =>
         {
-          this.setState({"total_num":res.data.total_num,
-                        "judged_num":res.data.judged_num
-          });
+          this.setState(
+            {"stats":res.data}
+          );
         }
     )
   }
+
   componentDidMount(){
       this.updateStatistic()
   }
@@ -51,42 +61,37 @@ class Form extends Component{
     this.props.handleFormChange(this.input.value);
   }
 
-  onhideBoilerPlate(event){
-    this.setState({'show':!this.state.show})
-    this.props.handleShowHide();
-  }
-
-
   render(){
     return (
-      <div>
-        <form onSubmit={this.handleFormChange}>
+      <div class='searchbar'>
             <input
-              type="text"
               ref={(input) => this.input = input}
-              onChange={(event) => {this.props.handleUrlChange(event.target.value)}}
-              style={{"width":"400px"}}
+              style={{"width":"500px"}}
               />
-          <input type="submit" value="Submit" />
-        </form>
-        <div class="buttons">
-          <input type="label" class="stats" value={"Total_#:" + this.state.total_num}/>
-          <input type="label" class="stats" value={"Judged#:" + this.state.judged_num}/>
-        </div>
-        <div class="buttons">
-              <input type="button" value="GotoUrl" onClick={(event) => {window.open(this.input.value)}}/>
-              <input type="button" value={this.state.show? "HideBoilerPlate" : "ShowBoilerPlate"} onClick={this.onhideBoilerPlate} ref={(input) => {this.hideInput = input}}/>
-        </div>
-        <div class="buttons">
-          <input type="button" value="RandomUrl(UnJudged)" onClick={()=>this.randomUrl(0)}/>
-          <input type="button" value="RandomUrl(Judged)" onClick={()=>this.randomUrl(1)}/>
-        </div>
-        <div class="buttons">
-          Signature:
-          <input type="text" ref="signature"/>
-          <input type="button" value="SaveJudge" onClick={this.props.handleSaveJudge}/>
-          <input type="button" value="LoadJudge" onClick={this.props.handleLoadJudge}/>
-        </div>
+          <input type="submit" value="Submit" class='button' onClick={this.handleFormChange}/>
+          <input type="button" value="GotoUrl" class='button' onClick={(event) => {window.open(this.input.value)}}/>
+            {
+                this.state.stats === null?
+                null:
+                <div style={{marginTop:10}} class="stats">
+                  <label>QuerySet:</label>
+                  <select onChange={(event)=>{this.setState({"querysetIdx":event.target.value})}}>
+                  {
+                      this.state.stats.querysets.map((queryset, qidx)=>
+                        (<option value={qidx}>{queryset.name}</option>)
+                      )
+                  }
+                  </select>
+                    <label>{"Total#:" + this.state.stats.querysets[this.state.querysetIdx].total}</label>
+                    <label>{"Judged#:" + this.state.stats.querysets[this.state.querysetIdx].judged}</label>
+                    <button class="button button2" onClick={()=>this.randomUrl(0)}>RandomUrl(Unjudged)</button>
+                    <br/>
+                    <label>Signature:</label>
+                    <input type="text" ref="signature" onChange={()=>{this.updateStatistic()}} style={{"width":"80px", "borderColor":"grey"}}/>
+                    <label>{"Judged_By_You#:" + this.state.stats.querysets[this.state.querysetIdx].judgedByYou}</label>
+                    <button onClick={()=>this.randomUrl(1)} class="button button2">RandomUrl(Judged)</button>
+                </div>
+            }
       </div>
     )
   }
@@ -102,13 +107,14 @@ class App extends Component {
       textunits:null,
       summary:null,
       labels:null,
-      show_boilerplate:true
+      judge:null,
+      last_update:null
     }
     this.handleFormChange = this.handleFormChange.bind(this);
     this.handleUrlChange = this.handleUrlChange.bind(this);
-    this.handleShowHide = this.handleShowHide.bind(this);
     this.handleSaveJudge = this.handleSaveJudge.bind(this);
     this.handleLoadJudge = this.handleLoadJudge.bind(this);
+    this.handleClearJudge = this.handleClearJudge.bind(this);
   }
 
   handleLoadJudge(event){
@@ -117,15 +123,32 @@ class App extends Component {
         return 
     }
     var json_d = {"url":this.state.url}
-    axios.post("/load_judge", json_d, headers).then(
+    axios.post(urlPrefix + "/load_judge", json_d, headers).then(
       (res) =>{
-          if (res.data.textunits !== null){
+          if (res.data && res.data.textunits){
               this.setState({"textunits":res.data.textunits, 
                             "labels": res.data.labels             
             })
           }
       })
   }
+
+  handleClearJudge(event){
+    var json_d = {"url":this.state.url}
+    axios.post(urlPrefix + "/clear_judge", json_d, headers).then(
+      (res) =>{
+          if (res.data.success)
+          {
+            this.setState({labels:null});
+            alert("[Succeed] Clear the judge")
+          }
+          else{
+            alert("[Failed] Please contact the dev.")
+          }
+          this.refs.form.updateStatistic()
+      })
+  }
+
 
   handleSaveJudge(event){
     if (this.state.url === ""){
@@ -134,13 +157,13 @@ class App extends Component {
     }
       //var data = JSON.stringify(this.refs.textunits.textunits_labels);
       //verify the judge first 
-      var judgeName = this.refs.form.refs.signature.value
+      var judgeName = this.refs.form.refs.signature.value.trim()
       if (!judgeName){
         alert("[Failed] Please Sign Your Name First!")
         return
       }
       var json_d = {"url":this.state.url, "textunits":this.state.textunits, "labels":this.refs.textunits.labels, "judge":judgeName}
-      axios.post("/save_judge", json_d, headers).then(
+      axios.post(urlPrefix + "/save_judge", json_d, headers).then(
           (res) =>{
               res.data.success?
                   alert("[Succeed] with name " + judgeName + "."):
@@ -160,7 +183,7 @@ class App extends Component {
             'Content-Type': 'application/json',
         }      
         var data = {url:url}
-        axios.post("/summary", data, headers)
+        axios.post(urlPrefix + "/summary", data, headers)
             .then(
                 res => {
 
@@ -168,18 +191,14 @@ class App extends Component {
                         textunits:res.data.textunits,
                         summary:res.data.summary,
                         url:url,
-                        labels:null,
-                        judged_num:res.data.judged_num,
-                        total_num:res.data.total_num
+                        labels:res.data.labels,
+                        judge:res.data.judge,
+                        last_update:res.data.last_update
                     });
                 }
             ).catch(err => {
                 console.log(err);
             });
-    }
-
-    handleShowHide = (event) => {
-        this.setState({show_boilerplate: !this.state.show_boilerplate})
     }
 
     render() {
@@ -189,18 +208,23 @@ class App extends Component {
             <h2 class='headline'> Deep Comparison</h2>
             <Form handleFormChange={this.handleFormChange} 
             handleUrlChange={this.handleUrlChange} 
-            handleShowHide={this.handleShowHide}
-            handleSaveJudge={this.handleSaveJudge}
-            handleLoadJudge={this.handleLoadJudge}
             ref="form"
             />
         </Row>
         <Row>
           <Col md={4} lg={4}>
-            <Paragraphs paragraphs={this.state.paragraphs} show_boilerplate={this.state.show_boilerplate}/>
+            <Paragraphs paragraphs={this.state.paragraphs}/>
           </Col>
           <Col md={4} lg={4}>
-            <TextUnits textunits={this.state.textunits} url={this.state.url} labels={this.state.labels} ref="textunits"/>
+            <TextUnits textunits={this.state.textunits} 
+                        url={this.state.url} 
+                        labels={this.state.labels}
+                        handleSaveJudge={this.handleSaveJudge}
+                        handleLoadJudge={this.handleLoadJudge}
+                        handleClearJudge={this.handleClearJudge}
+                        judge={this.state.judge}
+                        last_update={this.state.last_update}
+                        ref="textunits"/>
           </Col>
           <Col md={4} lg={4}>
             <Summary summary={this.state.summary}/>
--- demo/src/index.js
+++ demo/src/index.js
@@ -3,6 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const WrappedApp = () =>(
+	<MuiThemeProvider>
+	<App/>
+	</MuiThemeProvider>
+	)
+ReactDOM.render(
+	<WrappedApp />, document.getElementById('root'));
 registerServiceWorker();
